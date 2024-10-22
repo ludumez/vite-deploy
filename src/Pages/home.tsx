a99787aaa8e3f97f3f1a5026a1f28dfdd76f3f09
Lukas Dumez <ludumez@hotmail.com>
2024-10-22 05:33:41 +0300

    updatiing pages

--- src/Pages/home.tsx
+++
@@ -1,7 +0,0 @@
-export function Home(){
-    return(
-        <>
-        <h1>This is home page</h1>
-        </>
-    )
-}
--- src/Pages/page1.tsx
+++
@@ -1,7 +0,0 @@
-export function Page1(){
-    return(
-        <>
-        <h1>This is home page 1</h1>
-        </>
-    )
-}
