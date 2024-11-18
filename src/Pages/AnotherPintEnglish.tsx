da40310fb4fec2fdd0e5140ceb7ee0d0246b2273
Lukas Dumez <ludumez@hotmail.com>
2024-11-18 14:33:11 +0100

    Updating text

--- src/Pages/AnotherPintEnglish.tsx
+++ src/Pages/AnotherPintEnglish.tsx
@@ -1,10 +1,24 @@
 import Divider from '@mui/material/Divider';
-
+import Grid from '@mui/material/Grid2';
 
 export function AnotherPintEnglish() {
   return (
       <>     
- 
+  <div className='Card'> 
+             <Grid container>                  
+                   <Grid size={{ xs: 6, md: 6 }}>    
+                   <p>ludumez@hotmail.com</p>
+                   </Grid>  
+                   <Grid size={{ xs: 6, md: 6 }}> 
+                         <p></p>
+                         <div className='CardFooter'>
+                               <a className='aIcon' href="https://www.linkedin.com/in/lukas-dumez-988a1a234/" target="_blank">   <img className='Icon' src="https://raw.githubusercontent.com/ludumez/vite-deploy/refs/heads/main/src/assets/linkedin.jpg"></img></a>
+                               <a className='aIcon'href="mailto:ludumez@hotmail.com" target="_blank">  <img className='Icon' src="https://raw.githubusercontent.com/ludumez/vite-deploy/refs/heads/main/src/assets/email.jpg"></img></a>
+                         </div>   
+                   </Grid>
+             </Grid>  
+      <Divider variant="middle"/>
+      </div> 
        <p> </p>
        <div className='Separator'>
              <h2 className='Offset'>Another Pint</h2>
@@ -16,6 +30,8 @@ export function AnotherPintEnglish() {
                   <p>
                   Another Pint is currently under development, which means, i’m <b>not allowed to disclose any actual game screenshots or code that is used in the game. </b>
                   As such, in this page I will go over all the systems I took ownership of for Another Pint but wont show any real game examples. 
+                  </p>
+                  <p>
                   These are all <b>recreations and explanations</b> coming out of memory and experience.
                   </p>
             </div > 
@@ -36,11 +52,13 @@ export function AnotherPintEnglish() {
                   <p> 
                         I decided on a <b>data driven</b> approach, where a character is loaded from a series of data points. 
                         I decided to separate the data into two groups. <b>Visual data</b> and <b>Personality data. </b> 
-                        Visual data needs to be retrieved to rebuild the character's body. 
-                        Personality data is a bunch of values that are interpreted by other systems. 
+                  </p>
+                  <p>
+                        Visual data needs to be <b>retrieved </b>to rebuild the character's body. 
+                        Personality data is a bunch of values that are <b>interpreted </b>by other systems. 
                   </p>
             </div>
-            <img className='SmallerImage' src="https://raw.githubusercontent.com/ludumez/vite-deploy/refs/heads/main/src/assets/CharacterSeparation.jpg"></img>
+            <img className='SmallerImage' src="https://raw.githubusercontent.com/ludumez/vite-deploy/refs/heads/main/src/assets/CharacterSeparation.JPG"></img>
        </div> 
 
        <div className='Card'> 
@@ -48,17 +66,22 @@ export function AnotherPintEnglish() {
             <div className="CardBody">  
                   <p>
                   A key part of this data driven approach is being able to save selected prefabs we want to use for hair, beards or other accessories. 
-                  <b> Monobehaviours can’t be serialized</b> and, as such, so can’t prefabs and we can’t save them. To fix this we keep a list of all possible accessories a 
+                  <b> Monobehaviours can’t be serialized</b> and, as such, so can’t prefabs and we can’t save them. 
+                  </p>                        
+                  <p>
+                  To fix this we keep a list of all possible accessories a 
                   character can have and we save its <b>index in that list</b>. This also has the benefit of saving memory since we are saving less data.  
+                  </p>
+                  <p>
                   The main drawback of indexing the selected accessories is that changing the order of these in the list will change the characters aspect.
                   </p>     
-                  <img className='SmallerImage' src="https://raw.githubusercontent.com/ludumez/vite-deploy/refs/heads/main/src/assets/CharacterAspect.jpg"></img>
+                  <img className='SmallerImage' src="https://raw.githubusercontent.com/ludumez/vite-deploy/refs/heads/main/src/assets/CharacterAspect.JPG"></img>
                   <p>
                   We also want to add variety to the character via body shapes. To do this we make use of the meshes <b>blend shapes</b> (or shape keys in blender). 
                   We have 3 body types which can blend two at a time.
                   Each accessory will have blend shapes that need to be individually adapted to look proper with each body type.
                   </p>                   
-                  <img className='SmallerImage' src="https://raw.githubusercontent.com/ludumez/vite-deploy/refs/heads/main/src/assets/BlendShapeTest.jpg"></img>
+                  <img className='SmallerImage' src="https://raw.githubusercontent.com/ludumez/vite-deploy/refs/heads/main/src/assets/BlendShapeExample.gif"></img>
             </div>
        </div>       
        <div className='Card'> 
@@ -67,11 +90,12 @@ export function AnotherPintEnglish() {
                   <p>
                   A Character’s personality is much easier to handle since we are only saving values that need to be interpreted by other systems.
                   </p>     
-                  <img className='SmallerImage' src="https://raw.githubusercontent.com/ludumez/vite-deploy/refs/heads/main/src/assets/CharacterPersonality.jpg"></img>            
-
+                  <img className='SmallerImage' src="https://raw.githubusercontent.com/ludumez/vite-deploy/refs/heads/main/src/assets/CharacterPersonality.JPG"></img>            
+                  <br></br>
                   <p>
-                  Once we have generated these data points we can make them accessible very easily through the UI and modify them. 
-                  We also have the added benefit of making this data very easy to export and to inject into someone's game.
+                  Once we have generated these data points we can make them <b>accessible very easily through the UI and modify them. </b> 
+                  We also have the added benefit of making this data very easy to export and to inject into someone's game, 
+                  potentially letting people make their own characters and making them public.
                   </p>                   
             </div>
        </div>  
@@ -87,6 +111,21 @@ export function AnotherPintEnglish() {
                   With this approach it is very easy to generate a NPC on demand with a target personality without much overhead and then generate the actual body when it's needed.
                   </p>                   
             </div>
+       </div>   <Divider variant="middle"/>
+       <div className='Card'> 
+             <Grid container>                  
+                   <Grid size={{ xs: 6, md: 6 }}>    
+                   <p>ludumez@hotmail.com</p>
+                   </Grid>  
+                   <Grid size={{ xs: 6, md: 6 }}> 
+                         <p></p>
+                         <div className='CardFooter'>
+                               <a className='aIcon' href="https://www.linkedin.com/in/lukas-dumez-988a1a234/" target="_blank">   <img className='Icon' src="https://raw.githubusercontent.com/ludumez/vite-deploy/refs/heads/main/src/assets/linkedin.jpg"></img></a>
+                               <a className='aIcon'href="mailto:ludumez@hotmail.com" target="_blank">  <img className='Icon' src="https://raw.githubusercontent.com/ludumez/vite-deploy/refs/heads/main/src/assets/email.jpg"></img></a>
+                         </div>   
+                   </Grid>
+             </Grid> 
+ 
        </div>          
  
      </>
