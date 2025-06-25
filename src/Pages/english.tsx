86016b728ccc0c0634f152a1e3c10504280d0a34
ludumez <ludmez@hotmail.com>
2025-06-25 18:47:05 +0200

    updating page order

--- src/Pages/english.tsx
+++ src/Pages/english.tsx
@@ -7,11 +7,11 @@ import Masonry, {ResponsiveMasonry} from "react-responsive-masonry"
 const images = [
       {id: 1, src: "https://raw.githubusercontent.com/ludumez/vite-deploy/refs/heads/main/src/assets/AnotherPint.jpg", tags: ["Programmer"], to:'AnotherPint'},//Another Pint
       {id: 6, src: "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/2726060/26e8c16f3cbb5a458b60ffd663571d8fa06c724d/header.jpg?t=1748530564", tags: ["Programmer"], to:'FableHospital'},//Fable Hospital
-      {id: 4, src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQH3NblPFnmICGgAddAi41HRO0rgsSzkdjDhg&s", tags: ["QA"], to:'Unnanounced'},//Elf on the Shelf
+      {id: 2, src: "https://raw.githubusercontent.com/ludumez/vite-deploy/refs/heads/main/src/assets/RaceForTheArcticCaptura.jpg", tags: ["Programmer"], to:'RaceToTheArctic'},//Race to the Arctic
       {id: 3, src: "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1978210/header.jpg?t=1701706239", tags: ["QA"], to:'TheGrinch'},//Grinch
       {id: 5, src: "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/3212580/e191789d5852ed5c2c27183888ca58017b5378fb/header.jpg?t=1750407348", tags: ["QA"], to:'BratzRythmAndStyle'},//Bratz
       {id: 7, src: "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1709170/header.jpg?t=1726417223", tags: ["Other"], to:'ParadiseMarsh'},//Paradise Marsh
-      {id: 2, src: "https://raw.githubusercontent.com/ludumez/vite-deploy/refs/heads/main/src/assets/RaceForTheArcticCaptura.jpg", tags: ["Programmer"], to:'RaceToTheArctic'},//Race to the Arctic
+      {id: 4, src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQH3NblPFnmICGgAddAi41HRO0rgsSzkdjDhg&s", tags: ["QA"], to:'Unnanounced'},//Elf on the Shelf
 ];
 
 const allTags = ["Programmer","QA","Other"];
@@ -101,10 +101,13 @@ export function English() {
              <br></br>
             <ResponsiveMasonry columnsCountBreakPoints={{350: 1, 750: 2, 900: 3}} >
                 <Masonry>
+
                      {filteredImages.map(img => ( 
-                       <Link key={img.id} to={`/${img.to}`}>                 
-                        <img className={"HoverImage"} key={img.id} src={img.src} alt="" />  
-                        </Link>                        
+                       
+                              <Link key={img.id} to={`/${img.to}`}>                 
+                              <img className={"HoverImage"} key={img.id} src={img.src} alt="" />  
+                              </Link>                        
+                        
                   ))} 
                   </Masonry>
             </ResponsiveMasonry>           
