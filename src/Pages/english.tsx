5b729f851acada4da78379db9866a61207958cb8
Lukas <ludumez@hotmail.com>
2025-10-09 02:42:17 +0200

    more obligatori missing stuff

--- src/Pages/english.tsx
+++ src/Pages/english.tsx
@@ -1,20 +1,6 @@
 import Grid from '@mui/material/Grid2';
-import { useState } from "react";
 import Box from '@mui/material/Box';
 
-const images = [
-      { id: 9, src: "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/4023850/c4628317684bd8691849e3e7a4f9de92d5186513/header.jpg?t=1758115566", tags: ["Programmer"], to: 'Coasterama' },//Coasterama
-      { id: 1, src: "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/3150160/631beb9be026d46908dbb811c36afe82520c0903/header.jpg?t=1755693682", tags: ["Programmer"], to: 'AnotherPint' },//Another Pint
-      { id: 6, src: "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/2726060/26e8c16f3cbb5a458b60ffd663571d8fa06c724d/header.jpg?t=1748530564", tags: ["Programmer"], to: 'FableHospital' },//Fable Hospital
-      { id: 2, src: "https://raw.githubusercontent.com/ludumez/vite-deploy/refs/heads/main/src/assets/RaceForTheArcticCaptura.jpg", tags: ["Programmer"], to: 'RaceToTheArctic' },//Race to the Arctic
-      { id: 3, src: "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1978210/header.jpg?t=1701706239", tags: ["QA"], to: 'TheGrinch' },//Grinch
-      { id: 5, src: "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/3212580/e191789d5852ed5c2c27183888ca58017b5378fb/header.jpg?t=1750407348", tags: ["QA"], to: 'BratzRythmAndStyle' },//Bratz
-      { id: 4, src: "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/3210120/8f8af6f5bb5922a25d6c43612a5e15b1d0999795/header_spanish.jpg?t=1753278657", tags: ["QA"], to: 'Unnanounced' },//Elf on the Shelf
-      { id: 8, src: "https://img.itch.zone/aW1nLzIxODk1NTc1LnBuZw==/original/5jwl0K.png", tags: ["Programmer"], to: 'ItCouldBeWetter' },//It could be wetter
-      { id: 7, src: "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1709170/header.jpg?t=1726417223", tags: ["Other"], to: 'ParadiseMarsh' },//Paradise Marsh
-];
-
-const allTags = ["Programmer", "QA", "Other"];
 
 export function English() {
 
